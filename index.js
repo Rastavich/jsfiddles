d15d0fece716ad30927687da978492303b76dc9f
Isaac Hauser <isaac.hauser@live.com.au>
2021-08-05 17:34:03 +1000

    Update index.js
    Add updates from codesandbox

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
 // run `node index.js` in the terminal
 
-'use strict';
-
 const counts = [
-  '900,google.com',
-  '60,mail.yahoo.com',
-  '10,mobile.sports.yahoo.com',
-  '40,sports.yahoo.com',
-  '300,yahoo.com',
-  '10,stackoverflow.com',
-  '20,overflow.com',
-  '5,com.com',
-  '2,en.wikipedia.org',
-  '1,m.wikipedia.org',
-  '1,mobile.sports',
-  '1,google.co.uk'
+  "900,google.com",
+  "60,mail.yahoo.com",
+  "10,mobile.sports.yahoo.com",
+  "40,sports.yahoo.com",
+  "300,yahoo.com",
+  "10,stackoverflow.com",
+  "20,overflow.com",
+  "5,com.com",
+  "2,en.wikipedia.org",
+  "1,m.wikipedia.org",
+  "1,mobile.sports",
+  "1,google.co.uk"
 ];
 
-let list = [];
+let listMap = new Map([]);
 let domain = [];
-let subDomain = [];
 
 function calculateClicksByDomain(counts) {
   for (let i = 0; i < counts.length; i++) {
-    domain = counts[i].split(',');
-    // console.log(domain);
+    domain = counts[i].split(",");
+    // listMap.set(domain[1], parseInt(domain[0]));
 
-    list.push(domain[0], domain[1]);
-  }
+    let split = domain[1].split(".");
+    console.log(domain[1]);
+    let subDomain = checkSplit(split);
+    console.log(listMap.get(subDomain));
 
-  // console.log(list.length);
-  for (let j = 0; j < list.length; j++) {
-    console.log(list[j]);
-    // concatDomains(list[j].split('.'));
+    if (listMap.get(subDomain) === domain[1]) {
+      listMap.set(subDomain, parseInt(domain[0]));
+    } else {
+      listMap.set(domain[1], parseInt(domain[0]));
+    }
   }
+  console.log(listMap);
+  return listMap;
 }
 
-function concatDomains(domain) {
-  // console.log(domain);
-  for (let i = domain.length; i > 0; i--) {
-    let previousValue = domain[1];
-    let val = domain[1];
-    // console.log(previousValue);
+function checkSplit(dom) {
+  let previousValue;
+  for (let i = dom.length; i > 0; i--) {
+    if (previousValue != null) {
+      previousValue += "." + dom[i];
+    } else {
+      previousValue = dom[i];
+    }
   }
-  // console.log(domain);
+  return previousValue;
 }
 
 calculateClicksByDomain(counts);
